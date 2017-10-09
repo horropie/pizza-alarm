9fac3a68a7965076a135104d1d7ee78410f141e3
horropie <31204217+horropie@users.noreply.github.com>
2017-10-09 20:47:39 +0200

    private

--- pizzaalarm.py
+++ pizzaalarm.py
@@ -6,10 +6,10 @@ from time import sleep
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
- 
+
 # Phone Numbers go into this dictionary
-phoneNumberList = {"Tomi Getselev": 01638850610, "Jakob Hollweck": 015229603683}
- 
+phoneNumberList = {"Name1": 01234567890, "Name2": 09876543210}
+
 while True:
     for key, value in phoneNumberList.iteritems():
         pNumber = str(value)
@@ -18,37 +18,37 @@ while True:
         thepage = urllib.urlopen(url)
         soup = BeautifulSoup(thepage, "xml")
         print "Checking " + pNumberName + "..."
-        
+
         # Gatekeeper XML tag stored in a variable
         orderStatuses = str(soup.find('OrderStatuses').text)
-        
+
         # Gatekeeper
         if orderStatuses != "":
-            
+
             # Pertinent XML tags stored in variables
             startTime = str(soup.find('StartTime').text)
             orderDescription = str(soup.find('OrderDescription').text)
             print orderDescription
             # vvv Put additional XML tags code in between these comments vvv
- 
- 
+
+
             # ^^^ Put additional XML tags code in between these comments ^^^
-          
+
             # Email code
             sender = 'pythonpizzaalerts@gmail.com'
             # Put your email address in between the '' for receiver
             receiver = 'jakob.hollweck@gmail.com'
- 
+
             # Structuring the email
             msg = MIMEMultipart()
             msg['From'] = sender
             msg['To'] = receiver
             msg['Subject'] = "Your friend ordered pizza!"
- 
+
             # The email message
             body = str(pNumberName) + " ordered " + orderDescription + " at " + startTime
             msg.attach(MIMEText(body, 'plain'))
- 
+
             # Access sender email and send email
             mail = smtplib.SMTP('smtp.gmail.com', 587)
             mail.ehlo()
@@ -57,12 +57,12 @@ while True:
             message = msg.as_string()
             mail.sendmail(sender, receiver, message)
             mail.close()
- 
+
         else:
             pass
             print "No order for " + pNumberName
- 
-        # Be cautious if you edit this sleep statement. 
-        # Do not reduce the wait time too much. 
+
+        # Be cautious if you edit this sleep statement.
+        # Do not reduce the wait time too much.
         # We don't want to accidentally DDoS Dominos :)
         sleep(60)
